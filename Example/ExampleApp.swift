ce982fce6dfc9c72999ea3769390208a3b1b54a8
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-10-13 09:41:49 -0400

    Initial Commit

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Gustavo Anjos on 13/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
