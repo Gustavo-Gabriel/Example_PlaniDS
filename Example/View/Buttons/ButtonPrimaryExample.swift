087db8e379b7d3341ddeb0d792663827d883730a
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-11-02 21:00:36 -0400

    add ButtonPrimary With Icon

--- Example/View/Buttons/ButtonPrimaryExample.swift
+++ Example/View/Buttons/ButtonPrimaryExample.swift
@@ -4,8 +4,15 @@ import Plani_DS
 struct ButtonPrimaryExample: View {
     var body: some View {
         GeometryReader { geometry in
-            ButtonPrimary(title: "Action")
-                .padding(.horizontal, Spacing.small)
+            VStack {
+                ButtonPrimary(title: "Action")
+                    .padding(.horizontal, Spacing.small)
+
+                ButtonPrimary(title: "Action", icon: "person")
+                    .padding(.horizontal, Spacing.small)
+                
+                
+            }
         }
     }
 }
